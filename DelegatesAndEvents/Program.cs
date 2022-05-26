10d68f87142eecbb26307666dc5c69a1935ddb1f
Courtenay <hdsheena@users.noreply.github.com>
2022-05-26 12:32:10 -0700

    created a delegate that builds and runs
    no output, use debugger to see what its doing

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    public delegate int Func1(int a, int b);
+
+
+    class Program
+
+    {
+        public int result;
+
+        static void Main()
+        {
+
+            Func1 operation = new Func1(Add);
+
+
+            int result = DoSomeMath(1, 2, (a, b) => a + b); // 3
+            result = DoSomeMath(1, 2, (a, b) => a - b); // -1
+            result = DoSomeMath(1, 2, Add);
+
+
+        }
+        static int DoSomeMath(int a, int b, Func1 operation) => operation(a, b);
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
